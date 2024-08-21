3e80e07dd8d299fffc73b5a65be377e88c806a86
MichealScott <micheal@mail.com>
2024-08-21 22:37:55 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,91 @@
+// on house click
+document.getElementsByClassName("fa-house-damage")[0].onclick = function slideIn() {
+    document.getElementsByClassName("about-me")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("about-me")[0].style.transition = "1s";
+    document.getElementsByClassName("contact-us")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("contact-us")[0].style.transition = "1s";
+    document.getElementsByClassName("portfolio")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("portfolio")[0].style.transition = "1s";
+    document.getElementsByClassName("blog")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("blog")[0].style.transition = "1s";
+    document.getElementsByClassName("resume")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("resume")[0].style.transition = "1s";
+
+}
+
+// on user tie click
+document.getElementsByClassName("fa-user-tie")[0].onclick = function slideIn() {
+    document.getElementsByClassName("about-me")[0].style.transform = "translateX(-100%)";
+    document.getElementsByClassName("about-me")[0].style.transition = "2s";
+    document.getElementsByClassName("contact-us")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("contact-us")[0].style.transition = "1s";
+    document.getElementsByClassName("portfolio")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("portfolio")[0].style.transition = "1s";
+    document.getElementsByClassName("blog")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("blog")[0].style.transition = "1s";
+    document.getElementsByClassName("resume")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("resume")[0].style.transition = "1s";
+
+}
+
+// on envelope click
+document.getElementsByClassName("fa-envelope")[0].onclick = function slideIn() {
+    document.getElementsByClassName("contact-us")[0].style.transform = "translateX(-100%)";
+    document.getElementsByClassName("contact-us")[0].style.transition = "2s";
+    document.getElementsByClassName("about-me")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("about-me")[0].style.transition = "1s";
+    document.getElementsByClassName("portfolio")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("portfolio")[0].style.transition = "1s";
+    document.getElementsByClassName("blog")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("blog")[0].style.transition = "1s";
+    document.getElementsByClassName("resume")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("resume")[0].style.transition = "1s";
+
+}
+
+// onbreifcase click
+document.getElementsByClassName("fa-briefcase")[0].onclick = function slideIn() {
+    document.getElementsByClassName("portfolio")[0].style.transform = "translateX(-100%)";
+    document.getElementsByClassName("portfolio")[0].style.transition = "2s";
+    document.getElementsByClassName("about-me")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("about-me")[0].style.transition = "1s";
+    document.getElementsByClassName("contact-us")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("contact-us")[0].style.transition = "1s";
+    document.getElementsByClassName("blog")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("blog")[0].style.transition = "1s";
+    document.getElementsByClassName("resume")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("resume")[0].style.transition = "1s";
+
+}
+
+// on receipt click
+document.getElementsByClassName("fa-receipt")[0].onclick = function slideIn() {
+    document.getElementsByClassName("blog")[0].style.transform = "translateX(-100%)";
+    document.getElementsByClassName("blog")[0].style.transition = "2s";
+    document.getElementsByClassName("contact-us")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("contact-us")[0].style.transition = "1s";
+    document.getElementsByClassName("portfolio")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("portfolio")[0].style.transition = "1s";
+    document.getElementsByClassName("about-me")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("about-me")[0].style.transition = "1s";
+    document.getElementsByClassName("resume")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("resume")[0].style.transition = "1s";
+
+}
+
+// onclick phonebook
+document.getElementsByClassName("fa-address-book")[0].onclick = function slideIn() {
+    document.getElementsByClassName("resume")[0].style.transform = "translateX(-100%)";
+    document.getElementsByClassName("resume")[0].style.transition = "2s";
+    document.getElementsByClassName("contact-us")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("contact-us")[0].style.transition = "1s";
+    document.getElementsByClassName("portfolio")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("portfolio")[0].style.transition = "1s";
+    document.getElementsByClassName("about-me")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("about-me")[0].style.transition = "1s";
+    document.getElementsByClassName("blog")[0].style.transform = "translateX(100%)";
+    document.getElementsByClassName("blog")[0].style.transition = "1s";
+}
+
+// window.onload = document.getElementsByClassName("about-me")[0].style.transform = "translateX(-100%)";
+// window.onload = document.getElementsByClassName("contact-us")[0].style.transform = "translateX(-100%)";
